9bdab610cc1fb5ffa57a5d1d497c6c3d0b3bd751
Ramwsh <jimkrauz@gmail.com>
2025-07-06 15:48:48 +0700

    added update parser async tests with db integration

---
+++ src/main_app/RemTech.Solution/modules/parsers_management/RemTech.ParsersManagement.Core/Domains/ParsersDomain/Parsers/Compares/CompareParserWaitDays.cs
@@ -0,0 +1,20 @@
+﻿using RemTech.ParsersManagement.Core.Common.Primitives.Comparing;
+
+namespace RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers.Compares;
+
+public sealed class CompareParserWaitDays : ICompare
+{
+    private readonly bool _compare;
+
+    public CompareParserWaitDays(IParser parser, int related)
+        : this(parser.WorkSchedule().WaitDays(), related) { }
+
+    public CompareParserWaitDays(int current, int related)
+    {
+        _compare = current == related;
+    }
+
+    public bool Equality() => _compare;
+
+    public static implicit operator bool(CompareParserWaitDays compare) => compare._compare;
+}
---
+++ src/main_app/RemTech.Solution/modules/parsers_management/tests/RemTech.ParsersManagement.DataAccess.Adapter.Tests/Parsers/Features/UpdateParserTests.cs
@@ -0,0 +1,133 @@
+﻿using RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers;
+using RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers.Compares;
+using RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers.ValueObjects;
+using RemTech.ParsersManagement.Core.Domains.ParsersDomain.Ports;
+using RemTech.ParsersManagement.Tests.Library;
+using RemTech.Result.Library;
+
+namespace RemTech.ParsersManagement.DataAccess.Adapter.Tests.Parsers.Features;
+
+public sealed class UpdateParserTests : IClassFixture<DataAccessParsersFixture>
+{
+    private readonly DataAccessParsersFixture _fixture;
+
+    public UpdateParserTests(DataAccessParsersFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    private async Task Update_Parser_Wait_Days_Async_Success()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        int expectedWaitDays = 4;
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncSuccess(parser, waitDays: expectedWaitDays);
+        await using ParsersSource source = _fixture.ParsersSource();
+        Status<IParser> fromDb = await source.Find(parser.Identification().ReadName());
+        Assert.True(fromDb.IsSuccess);
+        Assert.True(new CompareParserWaitDays(fromDb.Value, expectedWaitDays));
+    }
+
+    [Fact]
+    private async Task Update_Parser_State_Async_Success()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        string expectedState = ParserState.Waiting();
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncSuccess(parser, state: expectedState);
+        await using ParsersSource source = _fixture.ParsersSource();
+        Status<IParser> fromDb = await source.Find(parser.Identification().ReadName());
+        Assert.True(fromDb.IsSuccess);
+        Assert.True(new CompareParserState(fromDb.Value, expectedState));
+    }
+
+    [Fact]
+    private async Task Update_Parser_Async_State_Failure()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        string invalidState = "Random text";
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncFailure(parser, state: invalidState);
+    }
+
+    [Fact]
+    private async Task Update_Parser_Wait_Days_Async_Failure()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        int invalidWaitDays = 10;
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncFailure(parser, waitDays: invalidWaitDays);
+    }
+
+    [Fact]
+    private async Task Update_Parser_Async_State_And_WaitDays_Failure()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        int invalidWaitDays = 10;
+        string invalidState = "Random text";
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncFailure(parser, state: invalidState, waitDays: invalidWaitDays);
+    }
+
+    [Fact]
+    private async Task Update_Parser_Working_Async_Failure()
+    {
+        string parserName = "Test Parser";
+        string parserType = "Техника";
+        string parserDomain = "Test";
+        string invalidState = ParserState.Working();
+        IParser parser = await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).AsyncAddNewParserSuccess(parserName, parserType, parserDomain);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncSuccess(parser, state: invalidState);
+        await new ParserTestingToolkit(
+            _fixture.Logger(),
+            _fixture.ParsersSource()
+        ).UpdateParserAsyncFailure(parser, state: invalidState);
+    }
+}
