9081be30335464e1a5aa28b70df4a3cae96359dd
Ramwsh <jimkrauz@gmail.com>
2025-07-06 15:31:09 +0700

    added disable parser async tests with db integration

---
+++ src/main_app/RemTech.Solution/modules/parsers_management/RemTech.ParsersManagement.Core/Domains/ParsersDomain/Parsers/Compares/CompareParserState.cs
@@ -0,0 +1,22 @@
+﻿using RemTech.ParsersManagement.Core.Common.Primitives.Comparing;
+using RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers.ValueObjects;
+
+namespace RemTech.ParsersManagement.Core.Domains.ParsersDomain.Parsers.Compares;
+
+public class CompareParserState : ICompare
+{
+    private readonly bool _compare;
+
+    public CompareParserState(IParser parser, string relatedState)
+        : this(parser.WorkState().Read().StringValue(), relatedState) { }
+
+    public CompareParserState(IParser parser, ParserState relatedState)
+        : this(parser.WorkState().Read().StringValue(), relatedState) { }
+
+    public CompareParserState(string parserState, string relatedState) =>
+        _compare = parserState == relatedState;
+
+    public bool Equality() => _compare;
+
+    public static implicit operator bool(CompareParserState compare) => compare._compare;
+}
