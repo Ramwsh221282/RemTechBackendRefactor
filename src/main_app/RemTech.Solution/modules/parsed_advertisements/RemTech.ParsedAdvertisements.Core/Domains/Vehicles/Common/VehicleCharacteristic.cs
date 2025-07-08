76a08847330c68cc9cb39db6053a5b5cf732f2dc
Ramwsh <jimkrauz@gmail.com>
2025-07-08 22:21:40 +0700

    added vehicle characteristic relation model

---
+++ src/main_app/RemTech.Solution/modules/parsed_advertisements/RemTech.ParsedAdvertisements.Core/Domains/Vehicles/Common/VehicleCharacteristic.cs
@@ -0,0 +1,33 @@
+﻿using RemTech.ParsedAdvertisements.Core.Domains.Vehicles.Characteristics;
+using RemTech.ParsedAdvertisements.Core.Domains.Vehicles.Transport;
+
+namespace RemTech.ParsedAdvertisements.Core.Domains.Vehicles.Common;
+
+public sealed class VehicleCharacteristic
+{
+    private readonly ParsedTransport _transport;
+    private readonly ParsedVehicleCharacteristic _characteristic;
+
+    public VehicleCharacteristic(
+        ParsedTransport transport,
+        ParsedVehicleCharacteristic characteristic
+    )
+    {
+        _transport = transport;
+        _characteristic = characteristic;
+    }
+
+    public ParsedTransport WhatTransport() => _transport;
+
+    public ParsedVehicleCharacteristic WhatCharacteristic() => _characteristic;
+
+    public static implicit operator ParsedTransport(VehicleCharacteristic vc)
+    {
+        return vc._transport;
+    }
+
+    public static implicit operator ParsedVehicleCharacteristic(VehicleCharacteristic vc)
+    {
+        return vc._characteristic;
+    }
+}
