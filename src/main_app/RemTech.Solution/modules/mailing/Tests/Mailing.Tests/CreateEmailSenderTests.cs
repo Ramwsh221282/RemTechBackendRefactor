12e9f921d4a1db50ee130b71dc5c6fab7857e1f8
Ramwsh221282 <jimkrauz@gmail.com>
2025-11-06 12:37:51 +0300

    added refactored method to write postmans

--- src/main_app/RemTech.Solution/modules/mailing/Tests/Mailing.Tests/CreateEmailSenderTests.cs
+++ src/main_app/RemTech.Solution/modules/mailing/Tests/Mailing.Tests/CreateEmailSenderTests.cs
@@ -20,9 +20,11 @@ public sealed class CreateEmailSenderTests(MailingTestServices services) : IClas
         CancellationToken ct = CancellationToken.None;
         PostmanDto dto = new PostmanDto();
         TaskCompletionSource<Status<Unit>> tcs = new();
+
         TestPostmanMetadata meta = new TestPostmanMetadata(Guid.NewGuid(), "test@mail.com", "test-password");
         TestPostmanStatistics stats = new(0, 0);
         TestPostman postman = new(meta, stats);
+
         WritePostmanInPostgres npgPostmanInPostgres = new(db, ct, tcs);
         WritePostmanInDto presenterPostmanInDto = new(dto);
         WritePostmanInteractive interactorPostmanInteractive = new(npgPostmanInPostgres, presenterPostmanInDto);
@@ -30,5 +32,20 @@ public sealed class CreateEmailSenderTests(MailingTestServices services) : IClas
         postman.Write(byDomainPostmanBy);
         Status<Unit> result = await tcs.Task;
         Assert.True(result.IsSuccess);
+    }
+
+    private async Task Test(PostmanDto dto, TaskCompletionSource<Status<Unit>> tcs, PostgresDatabase db,
+        CancellationToken ct)
+    {
+        // todo add cache.
+        var meta = new TestPostmanMetadata(Guid.NewGuid(), "test@mail.com", "test-password");
+        var stats = new TestPostmanStatistics(0, 0);
+        var postman = new TestPostman(meta, stats);
+        var writePg = new WritePostmanInPostgres(db, ct, tcs);
+        var writePresenter = new WritePostmanInDto(dto);
+        var writeInteractive = new WritePostmanInteractive(writePg, writePresenter);
+        var writeByDomain = new WritePostmanByDomain(writeInteractive);
+        postman.Write(writeByDomain);
+        var result = await tcs.Task;
     }
 }
