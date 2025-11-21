db5557721d20f331807f65984c0c6fb187041af1
Ramwsh221282 <jimkrauz@gmail.com>
2025-11-22 03:22:38 +0700

    added messages choreography test

---
+++ src/main_app/RemTech.Solution/modules/building_blocks/RemTech.Primitives.Extensions/Exceptions/MessageException.cs
@@ -0,0 +1,31 @@
+﻿namespace RemTech.Primitives.Extensions.Exceptions;
+
+public abstract class MessageException(string error) : Exception(error)
+{
+    public string Error => error;
+    public class ConflictException(string error) : MessageException(error);
+    public class NotFoundException(string error) : MessageException(error);
+    public class ValidationException(string error) : MessageException(error);
+    public class InternalException(string error) : MessageException(error);
+
+    public static void Conflict(string message)
+    {
+        throw new ConflictException(message);
+    }
+    
+    public static void NotFound(string message)
+    {
+        throw new NotFoundException(message);
+    }
+    
+    public static void Validation(string message)
+    {
+        throw new ValidationException(message);
+    }
+    
+    public static void Internal(string message)
+    {
+        throw new InternalException(message);
+    }
+    
+}
