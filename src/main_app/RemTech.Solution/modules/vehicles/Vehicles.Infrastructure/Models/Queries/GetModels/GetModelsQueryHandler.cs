3629d85a7788f2d6a388886cde7a4fe51f283252
Ramwsh221282 <jimkrauz@gmail.com>
2026-01-21 01:12:36 +0700

    Backend: Fix for models query. Now only returns brands of vehicles that are not deleted and vehicles that are in catalogue

--- src/main_app/RemTech.Solution/modules/vehicles/Vehicles.Infrastructure/Models/Queries/GetModels/GetModelsQueryHandler.cs
+++ src/main_app/RemTech.Solution/modules/vehicles/Vehicles.Infrastructure/Models/Queries/GetModels/GetModelsQueryHandler.cs
@@ -23,14 +23,19 @@ public sealed class GetModelsQueryHandler(NpgSqlSession session)
     {
         DynamicParameters parameters = new();
         List<string> filterSql = [];
+        filterSql.Add("i.deleted_at IS NULL");
         ApplyFilters(query, filterSql, parameters);
 
         string sql = $"""
             SELECT
                 m.id,
                 m.name                
-            FROM vehicles_module.models m            
-            {CreateWhereClause(filterSql)}                        
+            FROM vehicles_module.models m      
+            INNER JOIN vehicles_module.vehicles v ON v.model_id = m.id
+            INNER JOIN contained_items_module.contained_items i ON v.id = i.id      
+            {CreateWhereClause(filterSql)} 
+            GROUP BY m.id, m.name
+            HAVING COUNT(v.id) > 0                       
             """;
 
         return (parameters, sql);
